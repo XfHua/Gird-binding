b0699272132b69aef9daee4e3e7cb7df73df92f9
XfHua <jasonHuaxf@163.com>
2019-04-19 10:45:35 +0800

    123

---
+++ App286/App286/App286/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App286
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        int a = 0;
+        public PlayerDetails playermodel;
+        public MainPage()
+        {
+            InitializeComponent();
+  
+            DisplayDetails();
+        }
+
+        public void DisplayDetails()
+        {
+            playermodel = new PlayerDetails();
+            gridDisplay.BindingContext = playermodel;
+            a++;
+            playermodel.FullName = a.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayDetails();
+        }
+    }
+
+    public class PlayerDetails : INotifyPropertyChanged
+    {
+
+        string fullname;
+
+        public PlayerDetails( ) {
+       
+        }
+
+        public string FullName
+        {
+            set
+            {
+                if (fullname != value)
+                {
+                    fullname = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("FullName"));
+                    }
+                }
+            }
+            get
+            {
+                return fullname;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
